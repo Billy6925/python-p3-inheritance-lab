72121bf2341fbf4cdae065f8d5fc5252670082fa
Billy Eskein <billyeskein@gmail.com>
2025-02-21 17:16:25 +0300

    Update lib/user.py

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,13 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self,first_name,last_name):
+        super().__init__(first_name,last_name)
+        self.knowledge = ["Maths","Science"]
 
     def teach(self):
-        pass
+        if self.knowledge:
+            index = random.randint(0, len(self.knowledge) -1)
+            return self.knowledge[index]
+        else:
+            return "No list of knowledge"
